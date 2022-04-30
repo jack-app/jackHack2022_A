317d3f8931a870612b643df3d1f049cce3a628f8
H0R15H0 <shohori13@gmail.com>
2022-04-30 18:32:26 +0900

    debug modeにしてホットリロードを有効化した

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def handle_message(data):
     print('received message: ' + data)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', debug=True)
